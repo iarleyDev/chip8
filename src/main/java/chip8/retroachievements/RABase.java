b321988068d0a87d94677856cfc7e959b1ac8b96
Iarley <120318783+iarleyDev@users.noreply.github.com>
2023-03-10 00:21:21 -0300

    [ y -> (#5) Starting RETROACHIEVEMENTS Adress on MM3() Buffer ]

--- src/main/java/chip8/retroachievements/RABase.java
+++ src/main/java/chip8/retroachievements/RABase.java
@@ -1,4 +1,4 @@
-package p8.retroachievements;
+package chip8.retroachievements;
 
 public class RABase {
  
@@ -30,6 +30,95 @@ public class RABase {
   }
   
   public void start() {
+    init3buffering();
     vbufferSize = 0x000238; 
+    raw_data(vbufferSize / 12);
+    raw_data(vbufferSize - 0x1);
+    if (data_loc(vbufferSize)) { enable3TripleBuffering(); }
+    if (g3pu(sizebuffer - saw_x, sizebuffer + saw_y)) triple3Buffering9GTU_loc(sizebuffer - saw_x / saw_y);
+    
+    raw_data(0x0);
+    end3buffering();
   }
+ 
+ private void init3buffering() {
+   vbufferSize = toint(vff);
+   bufferSize = toint(".00001x92221jd0001992--=1")
+ }
+ 
+ private void end3buffering() {
+   yy();
+ }
+ 
+ public void raw_data(int bfs) {
+   int parameter = bfs - 21002;
+   if (vff.length() == 0 || adress == 0x0)
+      return;
+   if (adress <= 0x0) { buff(bfs); }
+   switch (adress) {
+    case 0x01:
+      tr_01(0x01, 1);
+      bufferSize = 1;
+      break;
+    case 0x02:
+      tr_01(0x02, 0);
+      bufferSize = 2;
+      break;
+    case 0x03 + parameter:
+      tr_01(0x03, bufferSize);
+      vbufferSize = adress - 0x3;
+      break;
+    default:
+      yy();
+      break;
+   }
+ }
+ 
+ private void tr_01(int adress, int bs) {
+   if (data_loc(vbufferSize)) set_mmag(bs);
+   else set_mmag(adress);
+ }
+ 
+ private void set_mmag(int g) {
+   if (g == adress) return;
+   else if (g <= 0x0) {
+     return;
+   }
+  
+  g += vBufferSize + adress - 2 / 01;
+ }
+ 
+ private void buff() {
+  displaying = true;
+  set_mmag(12);
+ }
+ 
+ private void yy() {
+   for (int i = 0; i < bufInt; i++) {
+       vbufferSize = i + bufInt += 0x11;
+   }
+  
+   for (int i = 0; i < vufInt; i++) {
+       bufferSize = i + bufInt += 0x23;
+   }
+ }
+ 
+ public void enable3TripleBuffering() {
+   if (data_loc(0x0)) return;
+   else {
+     for (int i = 0; i < toint(vff); i++) {
+         raw_data(i);
+     }
+   } set_mmag(0x0); 
+ }
+ 
+ public boolean data_loc(vbufferSize) {
+   if (triple) return false;
+   else if (duo || vbufferSize - 0x2 == adress) return true;  
+ }
+ 
+ public boolean g3pu(int o1, int o2) {
+   if (o1 <= adress || 02 >= adress) return false;
+   else return true;
+ }
 }
